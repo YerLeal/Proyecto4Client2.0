4f2c7265176a74b0af9e71018bd23dd559e96815
YerLeal <yeryazmin96@gmail.com>
2018-06-24 12:46:21 -0600

    Chat 1

--- proyecto4client2/MyClient.java
+++ proyecto4client2/MyClient.java
@@ -1,26 +1,20 @@
-package proyecto4client;
+package proyecto4client2;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
-import java.io.StringReader;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.swing.JOptionPane;
-import org.jdom.Document;
-import org.jdom.Element;
-import org.jdom.JDOMException;
-import org.jdom.input.SAXBuilder;
-import org.jdom.output.Format;
-import org.jdom.output.XMLOutputter;
 
 public class MyClient extends Thread {
 
     private int socketPortNumber;
+    private String action;
+    public static PrintStream send;
 
     public MyClient() {
         this.socketPortNumber = utilities.Constants.socketPortNumber;
@@ -32,13 +26,23 @@ public class MyClient extends Thread {
             InetAddress address = InetAddress.getByName(utilities.Constants.address);
             Socket socket = new Socket(address, this.socketPortNumber);
 
-            PrintStream send = new PrintStream(socket.getOutputStream());
-            BufferedReader receive = new BufferedReader(
-                    new InputStreamReader(socket.getInputStream())
-            );
-            
-            send.println("Yer");
+            send = new PrintStream(socket.getOutputStream());
+            BufferedReader receive = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
+            // 2 com: recibo nombre del server
+            System.out.println("The server sent me: " + receive.readLine());
+
+            send.println(action+"&Yer&Hola");
+            
+            if (this.action.equals("CHAT")) {
+                // 3 com: envio accion, nombre y mensaje
+                while (true) {
+                    String line = receive.readLine();
+                    if (line.startsWith("MESSAGE")) {
+                        System.out.println(line);
+                    }
+                }
+            }
             socket.close();
         } catch (UnknownHostException ex) {
             Logger.getLogger(MyClient.class.getName()).log(Level.SEVERE, null, ex);
@@ -46,5 +50,9 @@ public class MyClient extends Thread {
             Logger.getLogger(MyClient.class.getName()).log(Level.SEVERE, null, ex);
         }
     } // run
+
+    public void setAction(String action) {
+        this.action = action;
+    }
 
 } // end class
